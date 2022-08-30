c257410b6a9a27e9c87f93a2a719724f027dd73b
Nicolas Betancur <nicobetancurochoa@gmail.com>
2022-08-29 20:03:54 -0500

    Debugging Commit

--- movie/admin.py
+++ movie/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import InventarioDeBodega 
+
 
 # Register your models here.
-admin.site.register(InventarioDeBodega)
+
--- movie/models.py
+++ movie/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class InventarioDeBodega(models.Model):
+class InventarioBodega(models.Model):
     id= models.AutoField(primary_key = True)
     columnas = models.IntegerField(verbose_name='Columnas')
     filas = models.IntegerField(verbose_name='Filas')
@@ -9,6 +9,8 @@ class InventarioDeBodega(models.Model):
 
 
 
+
+#hay que dropear la tabla y volverla hacer ya que tenemos que modificar unos valores y cambiarle el nombre, tambien la tenemos que volve
     
 
 
